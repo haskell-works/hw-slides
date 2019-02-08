9e0bcd4e154f6dabfc6d5778e01cd0dc613e8c1f
John Ky <newhoggy@gmail.com>
2019-02-08 16:50:31 +1100

    Add Bézier example

--- src/HaskellWorks/Data/Slides.hs
+++ src/HaskellWorks/Data/Slides.hs
@@ -1,3 +1,7 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GADTs            #-}
+{-# LANGUAGE TypeFamilies     #-}
+
 module HaskellWorks.Data.Slides where
 
 import Diagrams.Backend.SVG
@@ -9,7 +13,26 @@ import qualified System.Directory as IO
 {-# ANN module ("HLint: ignore Redundant do"        :: String) #-}
 
 myDiagram :: Diagram B
-myDiagram = square 1 # fc blue ||| circle 1 # fc blue
+myDiagram = square 1 # fc blue ||| circle 1 # fc blue ||| example
+
+illustrateBézier c1 c2 x2
+    =  endpt
+    <> endpt  # translate x2
+    <> ctrlpt # translate c1
+    <> ctrlpt # translate c2
+    <> l1
+    <> l2
+    <> fromSegments [bézier3 c1 c2 x2]
+  where dashed  = dashingN [0.03, 0.03] 0
+        endpt   = circle 0.05 # fc red  # lw none
+        ctrlpt  = circle 0.05 # fc blue # lw none
+        l1      = fromOffsets [c1] # dashed
+        l2      = fromOffsets [x2 ^-^ c2] # translate c2 # dashed
+
+x2        = r2 (3, -1) :: V2 Double     -- endpoint
+[c1, c2]  = map r2 [(1, 2), (3, 0)]     -- control points
+
+example = illustrateBézier c1 c2 x2
 
 run :: IO ()
 run = do
